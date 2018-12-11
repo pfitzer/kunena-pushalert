a0ea7798e3a56d15174e1e172134de16b99180bf
Michael Pfister <m.pfister@t-systems.com>
2018-12-11 11:29:56 +0100

    first commit

---
+++ system/pushalert.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * @package    kunena-push-alert
+ *
+ * @author     A41646139 <your@email.com>
+ * @copyright  A copyright
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ * @link       http://your.url.com
+ */
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\Database\DatabaseDriver;
+
+defined('_JEXEC') or die;
+
+/**
+ * Kunena-push-alert plugin.
+ *
+ * @package  kunena-push-alert
+ * @since    1.0
+ */
+class plgSystemKunenaPushAlert extends CMSPlugin
+{
+	/**
+	 * Application object
+	 *
+	 * @var    CMSApplication
+	 * @since  1.0
+	 */
+	protected $app;
+
+	/**
+	 * Database object
+	 *
+	 * @var    DatabaseDriver
+	 * @since  1.0
+	 */
+	protected $db;
+
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  1.0
+	 */
+	protected $autoloadLanguage = true;
+
+	/**
+	 * onAfterInitialise.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterInitialise()
+	{
+		
+	}
+
+	/**
+	 * onAfterRoute.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRoute()
+	{
+	
+	}
+
+	/**
+	 * onAfterDispatch.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterDispatch()
+	{
+	
+	}
+
+	/**
+	 * onAfterRender.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRender()
+	{
+		// Access to plugin parameters
+		$sample = $this->params->get('apikey', '42');
+	}
+
+	/**
+	 * onAfterCompileHead.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterCompileHead()
+	{
+	
+	}
+
+	/**
+	 * OnAfterCompress.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterCompress()
+	{
+	
+	}
+
+	/**
+	 * onAfterRespond.
+	 *
+	 * @return  void.
+	 *
+	 * @since   1.0
+	 */
+	public function onAfterRespond()
+	{
+	
+	}
+
+    /**
+     * Get Kunena activity stream integration object.
+     *
+     * @return \KunenaActivityCommunity|null
+     * @since Kunena
+     */
+    public function onKunenaGetActivity()
+    {
+        require_once __DIR__ . "/push.php";
+        return new KunenaActivityCommunity($this->params);
+    }
+}
